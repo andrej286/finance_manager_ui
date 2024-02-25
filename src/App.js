9fd22eba1b2a869b33dc0fc421b45ea36edab677
andrej286 <mikistojanovski6@gmial.com>
2024-02-25 13:09:17 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React for the financial manager
         </a>
       </header>
     </div>
